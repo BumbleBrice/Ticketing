b1ea10920b31f6ef5402608e4dd612a044fb1d4c
Alexis <meunier_33@ive.fr>
2018-08-08 14:21:36 +0200

    aze

--- src/Controller/SpectacleController.php
+++ src/Controller/SpectacleController.php
@@ -22,6 +22,54 @@ class SpectacleController extends Controller
      */
     public function index(SpectacleRepository $spectacleRepository): Response
     {
+        $weezevent = new Weezevent('collilieux.brice@gmail.com', 'bumbleDev&2018', 'accbf05c0bc82872681e3c63eb9d0d4d');
+
+        $events = $weezevent->getEvents([
+            'include_not_published' => true,    
+            'include_closed'        => true,
+            'include_without_sales' => true]);
+
+        foreach($events->events as $event)
+        {
+            $eventDetail = $weezevent->getEventDetails($event->id);
+
+            $spectacle = $spectacleRepository->findOneBy(['weezevent_id' => $event->id]);
+
+            if($spectacle === null)
+            {
+                $spectacle = new Spectacle();
+                $spectacle->setNom($event->name);
+                
+                $date = new \DateTime($event->date->start);
+
+                $spectacle->setDate($date);
+                $spectacle->setPlaces($event->participants);
+                $spectacle->setDescription($eventDetail->events->description);
+                $spectacle->setWeezeventId($event->id);
+                $spectacle->setLastUpdate($eventDetail->last_update);
+                $spectacle->setPicture($eventDetail->events->image);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($spectacle);
+                $em->flush();
+            }
+            if($spectacle !== null && $spectacle->getLastUpdate() !== $eventDetail->last_update){
+                $spectacle->setNom($event->name);
+                
+                $date = new \DateTime($event->date->start);
+
+                $spectacle->setDate($date);
+                $spectacle->setPlaces($event->participants);
+                $spectacle->setDescription($eventDetail->events->description);
+                $spectacle->setWeezeventId($event->id);
+                $spectacle->setLastUpdate($eventDetail->last_update);
+                $spectacle->setPicture($eventDetail->events->image);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+            }
+        }
+        
         return $this->render('spectacle/index.html.twig', ['spectacles' => $spectacleRepository->findAll()]);
     }
 
@@ -100,7 +148,6 @@ class SpectacleController extends Controller
                 $em = $this->getDoctrine()->getManager();
                 $em->flush();
             }
-
         }
 
         return $this->render('spectacle/show.html.twig', ['spectacle' => $spectacleShow]);
